e26174097042f3a58ff057952603b1cb2150a696
Cesimar Xavier <jetompson@gmail.com>
2026-05-04 21:14:02 -0300

    feat: interface para consulta de usuário no banco

---
+++ src/main/java/br/edu/ifrn/agendamento/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ifrn.agendamento.repository;
+
+import br.edu.ifrn.agendamento.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+}
