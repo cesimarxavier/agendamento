80db204a6fdd00777bbee631f6482566b899eb96
Cesimar Xavier <jetompson@gmail.com>
2026-05-04 21:11:35 -0300

    feat: criando a entidade usuário

---
+++ src/main/java/br/edu/ifrn/agendamento/model/Usuario.java
@@ -0,0 +1,53 @@
+package br.edu.ifrn.agendamento.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoRole role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public TipoRole getRole() {
+        return role;
+    }
+
+    public void setRole(TipoRole role) {
+        this.role = role;
+    }
+
+}
