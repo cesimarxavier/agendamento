fb0622a447725da6810b4a62d1f95af5e79cba82
Cesimar Xavier <jetompson@gmail.com>
2026-05-04 21:16:07 -0300

    feat: implementando a interface UserDetailService, exigida pelo Spring Security para buscar dados persistido via JPA

---
+++ src/main/java/br/edu/ifrn/agendamento/service/AutenticacaoService.java
@@ -0,0 +1,31 @@
+package br.edu.ifrn.agendamento.service;
+
+import br.edu.ifrn.agendamento.model.Usuario;
+import br.edu.ifrn.agendamento.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    private final UsuarioRepository repository;
+
+    public AutenticacaoService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return User.builder()
+                .username(usuario.getUsername())
+                .password(usuario.getPassword())
+                .authorities(usuario.getRole().name())
+                .build();
+    }
+}
